1b2194eb2fad8dfc30d39382141f77e9d23dd31a
Libannur <99373712+Libannur@users.noreply.github.com>
2024-06-26 15:44:28 -0500

    added background image

--- components/buttons/loginButton.js
+++ components/buttons/loginButton.js
@@ -2,7 +2,7 @@ import { signIn } from '../../utils/auth';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<h3>Welcome to Madina Restraunt</h3><button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
